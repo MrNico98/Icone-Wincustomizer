38291ee06f8557bda0e7c82ca11ec267178a88f9
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2023-10-04 21:47:42 +0200

    Delete Office2019 directory

--- Office2019/Attivatore.bat
+++
@@ -1,7 +0,0 @@
-if exist "C:\Program Files\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files\Microsoft Office\Office16"
-if exist "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files (x86)\Microsoft Office\Office16"
-for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x"
-cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
-cscript ospp.vbs /sethst:kms.msgang.com
-cscript ospp.vbs /act
-pause
